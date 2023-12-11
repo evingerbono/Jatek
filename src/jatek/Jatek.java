9c6e0a2bf3def91469f271ba84eabb303f6dba63
evingerbono <bono0823@gmail.com>
2023-12-11 11:38:24 +0100

    Tárgy_használás_és_Karekter_mentés

--- src/jatek/Jatek.java
+++ src/jatek/Jatek.java
@@ -1,5 +1,11 @@
 package jatek;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Jatek {
 
@@ -7,10 +13,25 @@ public class Jatek {
 
         Targy t1 = new Targy("Lándzsa", "Fegyver");
         Targy t2 = new Targy("Poti", "Gyógyszer");
-        Karakter k1 = new Karakter((dobas() + dobas()),dobas(), dobas());
+        Karakter k1 = new Karakter((dobas() + dobas()), dobas(), dobas());
         System.out.println(k1.toString());
-        k1.Felvesz(t2);
+        k1.Felvesz(t1);
+        k1.Felvesz(t2, 4);
         System.out.println(k1.toString());
+        k1.Hasznal("Kard");
+        k1.Hasznal("Poti", 3);
+        //k1.Hasznal("Poti", 31);
+        k1.Hasznal("Lándzsa", 1, false);
+        //k1.Hasznal("Lándzsa", 1, true);
+        k1.Hasznal("Lándzsa", 5, true);
+
+        try (ObjectOutputStream objKi = new ObjectOutputStream(new FileOutputStream("karakter.bin"))) {
+            objKi.writeObject(k1);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Jatek.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Jatek.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
--- src/jatek/Karakter.java
+++ src/jatek/Karakter.java
@@ -10,26 +10,73 @@ public class Karakter {
         this.eletero = eletero;
         this.szerencse = szerencse;
         this.ugyesseg = ugyesseg;
+        this.felszereles = new ArrayList<>();
     }
 
-    public int getEletero() {
-        return eletero;
-    }
-
-    public int getSzerencse() {
-        return szerencse;
-    }
-
-    public int getUgyesseg() {
-        return ugyesseg;
-    }
-    
-    public void Felvesz(Targy nev){
+    public void Felvesz(Targy nev) {
         felszereles.add(nev);
     }
-    public void Felvesz(Targy nev, int db){
+
+    public void Felvesz(Targy nev, int db) {
         for (int i = 0; i < db; i++) {
             felszereles.add(nev);
+        }
+    }
+
+    private boolean Nins(String nev) {
+        for (Targy targy : felszereles) {
+            if (targy.getNev().equals(nev)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Hasznal(String nev) {
+        if (Nins(nev)==false) {
+            System.out.println("Nincs ilyen tárgy: " + nev);
+        } else {
+            for (int i = 0; i < felszereles.size(); i++) {
+                if (felszereles.get(i).getNev().equals(nev)) {
+                    felszereles.remove(i);
+                }
+            }
+            System.out.println("Használ " + nev);
+        }
+    }
+
+    public void Hasznal(String nev, int db) {
+        if (Nins(nev)==false) {
+            System.out.println("Nincs ilyen tárgy: " + nev);
+        } else {
+            int db2 = 0;
+            for (int i = 0; i < felszereles.size(); i++) {
+                if (felszereles.get(i).getNev().equals(nev)) {
+                    db2++;
+                }
+            }
+
+            if (db > db2) {
+                System.out.println("Nincs ennyi " + nev + " tárgyad");
+            } else {
+                System.out.println("Használ " + db + "db " + nev);
+                for (int i = 0; i < felszereles.size() && db2 > 0;) {
+                    if (felszereles.get(i).getNev().equals(nev)) {
+                        felszereles.remove(i);
+                        db2--;
+                    } else {
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+
+    public void Hasznal(String nev, int db, boolean eleg) {
+        if (eleg == false) {
+            System.out.println("Nem használtál tárgyat");
+        } else {
+            Hasznal(nev, db);
         }
     }
 
@@ -37,5 +84,4 @@ public class Karakter {
     public String toString() {
         return "Karakter{" + "eletero=" + eletero + ", szerencse=" + szerencse + ", ugyesseg=" + ugyesseg + ", felszereles=" + felszereles + '}';
     }
-    
 }
